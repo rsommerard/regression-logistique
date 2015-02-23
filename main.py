72cf2f105e074232a08e0895a0c24d10233df7d7
Romain SOMMERARD <rsommerard@gmail.com>
2015-02-23 22:07:43 +0100

    Une bonne descente

--- main.py
+++ main.py
@@ -1,61 +1,73 @@
 # -*- coding: utf8 -*-
 
+#Batch -> theta
+
+#matrix[:,0] -> tailles
+#matrix[:,1] -> poids
+
 import numpy
 from matplotlib import pyplot
 import random
 import math
 
 def load_data():
-  global Nt, Np, N, t, p, matrix
-  t = numpy.loadtxt('data/taillepoids_f.txt')
-  Nt = len(t)
-  p = numpy.loadtxt('data/taillepoids_h.txt')
-  Np = len(p)
-  matrix = numpy.concatenate((t, p), axis=0)
-  N = Nt + Np
+  global N, matrix, f, h, y, x
+  f = numpy.loadtxt('data/taillepoids_f.txt')
+  h = numpy.loadtxt('data/taillepoids_h.txt')
+  matrix = numpy.concatenate((f, h), axis=0)
+  N = len(matrix)
+  x = numpy.vstack((matrix[:,0], numpy.ones(N)))
+  y = matrix[:,1]
+
+def pas_batch(val):
+  A = 1.0
+  B = 10000
+  C = 1000
+  return ((A/(C + (B * val))))
+
+def f_theta(theta):
+  return numpy.dot(theta.T, x)
 
 def print_graphs():
   pyplot.figure(1)
-  pyplot.plot(t[:,0],t[:,1], '.')
-  pyplot.plot(p[:,0],p[:,1], '.')
-
-  #pyplot.figure(2)
-  #pyplot.plot(p, '.')
+  pyplot.plot(f[:,0],f[:,1], '.', label="femme")
+  pyplot.plot(h[:,0],h[:,1], '.', label="homme")
+  pyplot.plot(matrix[:,0], f_theta(batch_gradient_descent()[-1]), label="theta")
+  pyplot.legend()
 
   pyplot.show()
-  
-def pas_batch(val):
-  A = 100.0
-  B = 1
-  C = 10000
-  return ((A/(C + (B * val))))
-  
+
 def j_theta(theta):
-  tmp = (matrix[1,:] - numpy.dot(matrix[0,:].T, theta))
-  # numpy.dot(tmp.T, tmp) = le carré
+  tmp = (y - numpy.dot(x.T, theta))
   return ((1.0/N) * numpy.dot(tmp.T, tmp))
-  
+
 def batch_gradient_descent():
   theta = [1, 1]
+  bf = [theta]
 
   previous = j_theta(theta)
   current = previous + 1
 
   i = 1
-  while (previous != current):
+  while (abs(previous - current) > 10e-6):
     previous = current
-    theta = theta + (pas_batch(i) * (1.0/N) * numpy.dot(matrix[0,:], (matrix[1,:] - (1.0/(1 + (math.exp(numpy.dot(theta.T, matrix[0,:]))))))))
+    theta = theta + (pas_batch(i) * (1.0/N) * numpy.dot(x, (y - numpy.dot(x.T, theta))))
+    bf.append(theta)
     i+=1
     current = j_theta(theta)
 
   print "Batch theta = ", theta
-  return theta
+  return bf
+
+def sigmoid(x):
+  a = 1
+  b = 0
+  den = (1 + (numpy.exp(numpy.dot((-1), (a * x + b)))))
+  return (1.0 / den)
 
 def main():
   load_data()
-  batch_gradient_descent()
   print_graphs()
 
 if __name__ == '__main__':
   main()
-  
